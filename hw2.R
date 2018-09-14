2f4f36a7e06822732bf36e1da29dda81471fb709
atirvine88 <atirvine88@gmail.com>
2018-09-14 08:45:37 -0400

    2nd Commit: Tried adding reactive elements and outputs along with comments
    Nothing Works

--- hw2.R
+++ hw2.R
@@ -5,7 +5,9 @@ library(ggplot2)
 library(shiny)
 library(reshape2)
 library(data.table)
+library(DT)
 
+#Data Loading and Cleaning
 Allegheny.County.Jail.2018.raw <- read.csv("~/GitHub/Homework_2/7f5da957-01b5-4187-a842-3f215d30d7e8.csv")
 clean.data <- subset(Allegheny.County.Jail.2018.raw, select = -X_id) %>% na.omit(clean.data)
 levels(clean.data$Gender) <- c("Female", "Male")
@@ -14,22 +16,24 @@ levels(clean.data$Race) <- c("Not Reported", "Asian", "Black", "Hispanic", "Indi
 
 # Define UI for application using a fluid page layout
 ui <- fluidPage(
-          
-   # Application title is
-   
+# Layout of Basic Input          
   titlePanel("Allegheny County Jail Bookings for September "),
       tabsetPanel(
-         tabPanel("Gender Information",
+         tabPanel("Count Information",
                   fluidRow(
                     column(4,
-                           wellPanel(checkboxGroupInput("practice", label = "Options", choices = c("Female", "Male")))),
+                           wellPanel(radioButtons("gender", label = "Options", choices = c("Male", "Female")))),
                     column(8, wellPanel(plotOutput("gender.info"))))),
          tabPanel("Race",
                   fluidRow(
                     column(4,
-                            wellPanel(checkboxGroupInput("practice2", label = "Race", choices = c("Not Reported", "Asian", "Black",
+                            wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
                                                                                   "Hispanic", "Indian", "Unknown", 
-                                                                                  "White", "Mixed-race")))))),
+                                                                                  "White", "Mixed-race"), 
+                                                                                  selected = c("Black", "White", "Hispanic"),
+                                                                                    multiple = TRUE,
+                                                                                    selectize = TRUE)))),
+                  column(8, wellPanel(plotOutput("race.info")))),
          tabPanel("Downloads and Table",
                    fluidRow(column(4, wellPanel(downloadButton("new.download", label = "Download New File")))))
       )
@@ -37,11 +41,24 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  ddply(clean.data, ~ Race, summarize, mean = mean(Age.at.Booking))
-  output$gender.info <- renderPlot({ggplotly(Allegheny.County.Jail.2018.raw, aes(x = Gender)) + 
-      geom_bar() + 
-      geom_text(aes())
-  })  
+  #ddply(clean.data, ~ Race + Gender, summarize, mean = mean(Age.at.Booking))
+  #filter.data <- reactive({subset(clean.data, subset = c())})
+  #clean.data.2 <- melt(clean.data)
+  r.input <- reactive ({if (length(input$race) > 0 ) {
+    clean.data <- subset(clean.data, race %in% input$race)}
+    return(clean.data)})
+  
+  output$gender.info <- renderPlot({
+    
+    ggplot(clean.data, aes(x = input$gender)) + 
+      geom_bar()})
+  
+  output$race.info <- renderPlot({
+    race.data <- r.input
+    ggplot(race.data, aes(x = Gender)) + geom_bar()})
+      
+  
+  output$new.download <- downloadHandler()
 }
 
 # Run the application 
