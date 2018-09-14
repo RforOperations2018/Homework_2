7965a01589e0cbc961b14b57cbf53bd60637e30e
atirvine88 <atirvine88@gmail.com>
2018-09-14 13:10:13 -0400

    Rearrangement of Buttons along with functioning reacitve element

--- hw2.R
+++ hw2.R
@@ -19,46 +19,79 @@ ui <- fluidPage(
 # Layout of Basic Input          
   titlePanel("Allegheny County Jail Bookings for September "),
       tabsetPanel(
-         tabPanel("Count Information",
+         tabPanel("Basic Information",
                   fluidRow(
                     column(4,
+                           wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
+                                                                                     "Hispanic", "Indian", "Unknown", 
+                                                                                     "White", "Mixed-race"), 
+                                                 selected = c("Black", "White", "Hispanic"),
+                                                 multiple = TRUE,
+                                                 selectize = TRUE)),
                            wellPanel(radioButtons("gender", label = "Options", choices = c("Male", "Female")))),
-                    column(8, wellPanel(plotOutput("gender.info"))))),
-         tabPanel("Race",
-                  fluidRow(
-                    column(4,
-                            wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
-                                                                                  "Hispanic", "Indian", "Unknown", 
-                                                                                  "White", "Mixed-race"), 
-                                                                                  selected = c("Black", "White", "Hispanic"),
-                                                                                    multiple = TRUE,
-                                                                                    selectize = TRUE)))),
-                  column(8, wellPanel(plotOutput("race.info")))),
+                    column(8, wellPanel( plotOutput("race.info"), plotOutput("gender.info"))))),
+                  # fluidRow(
+                  #   column(4,
+                  #          wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
+                  #                                                                    "Hispanic", "Indian", "Unknown", 
+                  #                                                                    "White", "Mixed-race"), 
+                  #                                selected = c("Black", "White", "Hispanic"),
+                  #                                multiple = TRUE,
+                  #                                selectize = TRUE)))),
+                  # column(8, offset =  4, wellPanel(plotOutput("race.info")))),
+         # tabPanel("Race",
+         #          fluidRow(
+         #            column(4,
+         #                    wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
+         #                                                                          "Hispanic", "Indian", "Unknown", 
+         #                                                                          "White", "Mixed-race"), 
+         #                                                                          selected = c("Black", "White", "Hispanic"),
+         #                                                                            multiple = TRUE,
+         #                                                                            selectize = TRUE)))),
+         #          column(8, wellPanel(plotOutput("race.info")))),
          tabPanel("Downloads and Table",
-                   fluidRow(column(4, wellPanel(downloadButton("new.download", label = "Download New File")))))
-      )
-)
+                   fluidRow(column(4, wellPanel(downloadButton("new.download", label = "Download New File")),
+                                      wellPanel(actionButton("click", "Click to See What Happens"))),
+                            column(8, wellPanel(renderDT("table", "Basic Data Table of September Bookings")))))
+      ))
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   #ddply(clean.data, ~ Race + Gender, summarize, mean = mean(Age.at.Booking))
   #filter.data <- reactive({subset(clean.data, subset = c())})
   #clean.data.2 <- melt(clean.data)
-  r.input <- reactive ({if (length(input$race) > 0 ) {
-    clean.data <- subset(clean.data, race %in% input$race)}
-    return(clean.data)})
-  
+  df.filter <- reactive ({
+    clean.data %>% filter(input$gender == Gender & input$race %in% Race)})
+   
+   
+    # # if (length(input$race) > 0 ) {
+    # clean.data <- subset(clean.data, race %in% input$race)}
+    # return(clean.data)})
+
+  output$race.info <- renderPlot({
+    df2 <- df.filter()
+    ggplot(df2, aes(x = Race, fill = Race)) + geom_bar()})
+
   output$gender.info <- renderPlot({
-    
-    ggplot(clean.data, aes(x = input$gender)) + 
+    df1 <- df.filter()
+    ggplot(df1, aes(x = Gender, fill = Gender)) + 
       geom_bar()})
   
-  output$race.info <- renderPlot({
-    race.data <- r.input
-    ggplot(race.data, aes(x = Gender)) + geom_bar()})
       
   
-  output$new.download <- downloadHandler()
+  output$new.download <- downloadHandler(
+    filename = function(){ 
+      paste("newdownload", Sys.date(), ".csv", sep = "" )},
+    content = function(file) {
+      write.csv(clean.data, file)
+    })
+  
+  output$table <- renderDataTable({
+     clean.data
+    
+    subset(starwars, select = c(name, height, mass, birth_year, homeworld, species, films))
+  })
 }
 
 # Run the application 
