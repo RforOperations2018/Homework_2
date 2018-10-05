444583c8fb7fd266c4f425e491a7fbd3eceea440
atirvine88 <atirvine88@gmail.com>
2018-10-05 11:08:27 -0400

    Change of function names; deletion of white space and comments; creation of URL; deletion of merged code from previous commit

--- hw2.R
+++ hw2.R
@@ -11,9 +11,9 @@ library(jsonlite)
 library(plotly)
 library(htmltools)
 
-<<<<<<< HEAD
+
 ## Creation of
-ckansql <- function(url) {
+ckanSQL <- function(url) {
   # Make the Request
   r <- RETRY("GET", URLencode(url))
   # Extract Content
@@ -25,25 +25,17 @@ ckansql <- function(url) {
 }
 #https://data.wprdc.org/datastore/dump/7f5da957-01b5-4187-a842-3f215d30d7e8
 # Unique values for Resource Field
-ckan_unique <- function(id, field) {
+ckanUnique <- function(id, field) {
   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
-  c(ckansql(URLencode(url)))
+  c(ckanSQL(URLencode(url)))
 }
-
-
-gender_choices <- sort(ckan_unique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Gender")$Gender)
-race_choices <- sort(ckan_unique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Race")$Race)
+#Create choices for fields of race and Gender
+gender_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Gender")$Gender)
+race_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Race")$Race)
 #gender <- mutate(gender, )
-#
-gender_choices <- recode(gender, F = 'Female',  M = 'Male' )
+#Relabel choices of gender and race (input in user interface)
+gender_choices <- recode(gender_choices, F = 'Female',  M = 'Male' )
 race_choices <- recode(race_choices, A = "Asian", B = "Black", H = "Hispanic", I = "Indian", U = "Unknown", W = "White", x = "Unreported")
-=======
-#Data Loading and Cleaning
-Allegheny.County.Jail.2018.raw <- read.csv("./downloadfile_hw2.csv")
-clean.data <- subset(Allegheny.County.Jail.2018.raw, select = -X_id) %>% na.omit(clean.data)
-levels(clean.data$Gender) <- c("Female", "Male")
-levels(clean.data$Race) <- c("Not Reported", "Asian", "Black", "Hispanic", "Indian", "Unknown", "White", "Mixed-race")
->>>>>>> parent of 2c8aa83... Changes made to change actual choices.
 
 
 # Define UI for application using a fluid page layout
@@ -55,21 +47,19 @@ ui <- fluidPage(
         tabPanel("Basic Information",
                   fluidRow(
                     column(4,
-<<<<<<< HEAD
-                           wellPanel(selectInput("race", label = "Race", choices = race_choices, 
-                                                 selected = c("Black", "White", "Hispanic"),
-                                                 multiple = TRUE,
+
+                           wellPanel(selectInput("race", label = "Race", 
+                                                 choices = list('Asian' = 'A',
+                                                                 'Black' = 'B',
+                                                                  'Hispanic' = 'H',
+                                                                    'Indian' = 'I',
+                                                                     'Unknwon' = 'U',
+                                                                     'White' = 'W',
+                                                                     'Unreported' = 'x'),
+                                                 selected = c("A"),
+                                                 multiple = FALSE,
                                                  selectize = TRUE)),
-                           wellPanel(radioButtons("gender", label = "Options", choices = gender_choices)),
-=======
-                           wellPanel(selectInput("race", label = "Race", choices = c("Not Reported", "Asian", "Black",
-                                                                                     "Hispanic", "Indian", "Unknown", 
-                                                                                     "White", "Mixed-race"), 
-                                                 selected = c("Black", "White", "Hispanic"),
-                                                 multiple = TRUE,
-                                                 selectize = TRUE)),
-                           wellPanel(radioButtons("gender", label = "Options", choices = c("Male", "Female"))),
->>>>>>> parent of 2c8aa83... Changes made to change actual choices.
+                           wellPanel(radioButtons("gender", label = "Options", choices = gender_choices, choiceValues = c("F", "M"))),
                            wellPanel(actionButton("click", "Click to See What Happens"))),
                    column(8, wellPanel( plotlyOutput("race.info")), 
                              wellPanel(plotlyOutput("gender.info"))))),
@@ -79,53 +69,31 @@ ui <- fluidPage(
                             column(8, wellPanel(dataTableOutput("table", width = "80%")))))
       ))
 
-
 # Define server logic required to draw two bar graphs, a data table, and a download button
 server <- function(input, output, session = session) {
 # create reactive element  
 df.filter <- reactive ({
-<<<<<<< HEAD
-
-  #Problem (hypothetical): matching input values, newly recoded, with API data within SQL
-  #Option 1 (fail): recode values from user input back to original raw form before API call
-  #Option 2 (fail): recode values from user input within url assignment
-  #Gender <- recode(input$gender, Female = "F", Male = "M")
-  #Race <- recode(input$race, Asian = "A", Black = "B", Hispanic = "H", Indian = "I", Unknown = "U", White  = "W", Unreported = "x")
-  #Option 1: Recode inputs as original values to 
-  
-  url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%27", 
-            input$gender ,"%27%20=%20%22Gender%22%20AND%20%22Race%22%20IN%20%27", 
-           input$race, "%27%20")
-  
-  # Load and clean data
-  clean.data <- ckansql(url) #%>%
-  #  mutate(Gender = ifelse(Gender == "F", "Female", "Male"), Race = recode( A = "Asian", B = "Black", H = "Hispanic", I = "Indian", U = "Unknown", W = "White", x = "Not reported"))
-  
-  return(clean.data)
+   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20%3D%20%27", 
+                input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
+     # use ckan on url and make clean data
+  clean.data <- ckanSQL(url) %>% na.omit() 
+  #clean.data <- recode(clean.data$race , A = "Asian", B = "Black", H = "Hispanic", I = "Indian", U = "Unknown", W = "White", x = "Not reported")mutate(Gender = ifelse(Gender == "F", "Female", "Male"), Race = recode( A = "Asian", B = "Black", H = "Hispanic", I = "Indian", U = "Unknown", W = "White", x = "Not reported"))
   #clean.data %>% filter(input$gender == Gender & Race %in% input$race)
-  })
-
-#Create api ask
-##https://data.wprdc.org/api/3/action/datastore_search_sql
-=======
-clean.data %>% filter(input$gender == Gender & Race %in% input$race)})
- 
->>>>>>> parent of 2c8aa83... Changes made to change actual choices.
+  return(clean.data)
+   })
 #Create Interactive Race and Gender Plot  
    output$race.info <- renderPlotly({
     df2 <- df.filter()
     # ggplots are easier to read when each plus starts a new line.
-    ggplotly(ggplot(df2, aes(x = Race, fill = Race)) + 
+    ggplotly(ggplot(df2, aes(x = Race , fill = Race)) + 
                geom_bar() + 
                ggtitle("Arrests by Race and Gender for September") +
                ylab("Total"))})
-
 #Create Interactive Gender Plot
   output$gender.info <- renderPlotly({
     df1 <- df.filter()
     ggplotly(ggplot(df1, aes(x = Gender, fill = Gender)) + 
       geom_bar()+ ggtitle("Arrests by Sex for September") + ylab("Total"))})
-  
 #Create Download Button that allows people to download info
   output$new.download <- downloadHandler(
     filename = function(){ 
@@ -144,7 +112,6 @@ clean.data %>% filter(input$gender == Gender & Race %in% input$race)})
     showNotification("You have successfully reset the filters for race", type = "message")
       })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
 
